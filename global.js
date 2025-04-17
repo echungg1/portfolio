5cd4a4f59caa298dcb88dfc19779157f7ee8817b
echungg1 <165755356+echungg1@users.noreply.github.com>
2025-04-17 16:31:39 -0700

    finished

--- global.js
+++ global.js
@@ -22,8 +22,8 @@ let pages = [
   ];
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-? "/"                  // Local server
-: "/portfolio/";         // GitHub Pages repo name
+? "/"
+: "/portfolio/"; 
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
@@ -32,7 +32,6 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
 
-    // next step: create link and add it to nav
     if (!url.startsWith('http')) {
         url = BASE_PATH + url;
       }
@@ -41,11 +40,6 @@ for (let p of pages) {
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
-    // nav.append(a);
-
-    // if (a.host === location.host && a.pathname === location.pathname) {
-    //     a.classList.add('current');
-    //   }
 
     a.classList.toggle(
     'current',
